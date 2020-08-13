c864a954d5cc7b50baf8c742aba965ad3643b4c3
fnzr <5471818+fnzr@users.noreply.github.com>
2020-08-12 23:01:01 -0300

    Weird WIP

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,120 @@ use std::rc::Rc;
 pub mod helpers;
 pub mod nodes;
 
-//pub type Node = Rc<RefCell<NodeWrapper>>;
-pub type Node = NodeWrapper;
+pub struct ActionNode {
+    status: Status,
+    tick: Box<dyn FnMut() -> Status>,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum NodeType {
+    Sequence,
+    Action,
+}
+
+pub type NodeKey = (NodeType, u16);
+
+pub type EventEntry = (NodeKey, Option<NodeKey>);
+
+pub type Node = Rc<RefCell<NodeWrapper>>;
+//pub type Node = NodeWrapper;
+//type Tree = (Vec<NodeWrapper>, VecDeque<u16>);
+
+pub struct Seq {
+    children: Vec<NodeKey>,
+    current_child: u16,
+    status: Status,
+}
+
+impl Seq {
+    pub fn initialize(&mut self, events: &mut VecDeque<EventEntry>, self_key: NodeKey) {
+        if let Some(child) = self.children.get(0) {
+            events.push_back((child.clone(), Some(self_key)));
+            self.status = Status::Running;
+        } else {
+            self.status = Status::Failure;
+        }
+    }
+
+    pub fn on_child_complete(
+        &mut self,
+        result: Status,
+        events: &mut VecDeque<EventEntry>,
+        self_key: NodeKey,
+    ) -> Status {
+        match result {
+            Status::Success => {
+                self.current_child += 1;
+                if let Some(child) = self.children.get(self.current_child as usize) {
+                    events.push_back((child.clone(), Some(self_key)));
+                    Status::Running
+                } else {
+                    Status::Success
+                }
+            }
+            Status::Failure => Status::Failure,
+            _ => panic!("Invalid result: {:?}", &result),
+        }
+    }
+}
+
+pub struct Tree {
+    actions: Vec<ActionNode>,
+    sequences: Vec<Seq>,
+    events: VecDeque<EventEntry>,
+    bt: BehaviorTree,
+}
+
+impl Tree {
+    pub fn step(&mut self) {
+        if let Some((node_key, opt_parent_key)) = self.events.pop_front() {
+            let (node_type, node_index) = &node_key;
+            let result = match node_type {
+                NodeType::Sequence => {
+                    let seq = self.sequences[node_index.clone() as usize];
+                    if seq.status != Status::Running {
+                        seq.initialize(&mut self.events, node_key)
+                    }
+                    seq.status
+                }
+                NodeType::Action => {
+                    let act = self.actions[node_index.clone() as usize];
+                    (act.tick)()
+                }
+            };
+            if result == Status::Success || result == Status::Failure {
+                if let Some(parent_key) = opt_parent_key {
+                    let (parent_type, parent_index) = &parent_key;
+                    match parent_type {
+                        NodeType::Sequence => {
+                            let seq = self.sequences[parent_index.clone() as usize];
+                            seq.on_child_complete(result, &mut self.events, parent_key);
+                        }
+                        _ => panic!("Unsuported parent type: {:?}", &parent_type),
+                    }
+                }
+            } else {
+                self.events.push_back((node_key, opt_parent_key));
+            }
+        }
+    }
+
+    pub fn action(&mut self, action: ActionNode) -> NodeKey {
+        let key = self.actions.len();
+        self.actions.push(action);
+        (NodeType::Action, key as u16)
+    }
+
+    pub fn sequence(&mut self, children: Vec<NodeKey>) -> NodeKey {
+        let key = self.sequences.len();
+        self.sequences.push(Seq {
+            children,
+            current_child: 0,
+            status: Status::Invalid,
+        });
+        (NodeType::Sequence, key as u16)
+    }
+}
 
 pub trait Behavior {
     fn initialize(&mut self, bt: &mut BehaviorTree) -> Status;
@@ -47,12 +159,6 @@ impl NodeWrapper {
 
     fn tick(&mut self, bt: &mut BehaviorTree) -> &Status {
         self.status = self.behavior.tick(bt);
-        if self.status == Status::Success || self.status == Status::Failure {
-            if let Some(index) = self.parent_index {
-                let mut parent = &bt.nodes[index as usize];
-                parent.on_child_complete(&self.status, bt);
-            }
-        }
         &self.status
     }
 
@@ -63,46 +169,6 @@ impl NodeWrapper {
 
     fn on_child_complete(&mut self, status: &Status, bt: &mut BehaviorTree) {
         self.status = self.behavior.on_child_complete(status, bt)
-    }
-}
-
-pub struct TreeBuilder {
-    next_index: u16,
-    nodes: Vec<Node>,
-}
-
-impl TreeBuilder {
-    pub fn new() -> Self {
-        Self {
-            next_index: 0,
-            nodes: vec![],
-        }
-    }
-
-    pub fn builder(mut self) -> BehaviorTree {
-        self.nodes.shrink_to_fit();
-        BehaviorTree::new(self.nodes)
-    }
-
-    pub fn action(mut self, behavior: Box<dyn Behavior>) -> Self {
-        self.nodes.push(NodeWrapper::new(behavior, None));
-        self.next_index += 1;
-        self
-    }
-
-    pub fn sequence(mut self, children: Vec<u16>) -> Self {
-        for child_rc in children.iter() {
-            self.nodes[child_rc.clone() as usize].parent_index = Some(self.next_index);
-        }
-        self.nodes.push(NodeWrapper::new(
-            Box::new(nodes::Sequence {
-                children: children,
-                current_child: 0,
-            }),
-            None,
-        ));
-        self.next_index += 1;
-        self
     }
 }
 
@@ -122,26 +188,35 @@ impl BehaviorTree {
     pub fn start(&mut self) {
         self.events.clear();
         let mut root = self.nodes.last().unwrap().clone();
-        root.initialize(self);
-        self.events.push_back(self.nodes.len() as u16);
+        //root.initialize(self);
+        //self.events.push_back(self.nodes.len() as u16);
     }
 
     pub fn run(&mut self) -> Status {
-        self.start();
-        while self.step() {}
-        self.nodes.last().unwrap().status
+        //self.start();
+        //while self.step() {}
+        //self.nodes.last().unwrap().status
+        Status::Invalid
     }
 
-    pub fn step(&mut self) -> bool {
+    pub fn node(&self, index: &u16) -> Node {
+        self.nodes[index.clone() as usize].clone()
+    }
+
+    pub fn step(&mut self, tree: &mut Tree) -> bool {
         if let Some(index) = self.events.pop_front() {
-            let mut node = self.nodes.get(index as usize).unwrap().clone();
+            let node_rc = self.nodes[index as usize].clone();
+            let mut node = node_rc.borrow_mut();
+            if node.status == Status::Invalid {
+                node.initialize(self);
+            }
             if node.status == Status::Aborted {
                 return true;
             }
             let status = node.tick(self);
             if status == &Status::Running {
                 //drop(node);
-                self.events.push_back(index);
+                tree.1.push_back(index);
             }
             true
         } else {
@@ -153,6 +228,7 @@ impl BehaviorTree {
 #[derive(PartialEq, Copy, Clone, Debug)]
 pub enum Status {
     Invalid,
+    Delegated,
     Running,
     Success,
     Failure,
